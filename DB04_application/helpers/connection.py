c651856f900590920dcf674fd748e1b2f96e7db3
2021006317 <eve7777@hanyang.ac.kr>
2024-11-11 11:49:05 +0900

    password env 설정

--- DB04_application/helpers/connection.py
+++ DB04_application/helpers/connection.py
@@ -1,11 +1,17 @@
 import psycopg2 as pg
+import os
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
 
 conn = None
+#. TODO: Override connection info
+db_password = os.getenv("DB_PASSWORD")
+print(db_password)
 
-# TODO: Override connection info
-
-db_connection_str = "host=localhost user=postgres dbname=assignment3 password=0000 port=5432"
-
+db_connection_str = f"host=localhost user=postgres dbname=assignment3 password={db_password} port=5432"
+print(db_connection_str)
 try:
     conn = conn = pg.connect(db_connection_str)
 except Exception as err:
